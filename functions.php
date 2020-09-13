7fa4b47c40ee831eb2e5c49bc903cb273c97ea72
ThembaLucasNgubeni <thembangubeni04@gmail.com>
2020-09-13 14:01:40 +0200

    Added project posts type and featured image support.

--- functions.php
+++ functions.php
@@ -1,1 +1,28 @@
 <?php
+
+// Add thumbnails support
+function posts_thumbnails() {
+    add_theme_support( 'post-thumbnails');
+}
+add_action('after_setup_theme', 'posts_thumbnails');
+
+// Add custom product posts
+function products_custom_post_type()
+{
+    register_post_type('project', [
+        'rewrite' => ['slug' => 'projects'],
+        'labels' => [
+            'name' => 'Projects',
+            'singlar_name' => 'Project',
+            'add_new_item' => 'Add New Project',
+            'edit_item' => 'Edit Project'
+        ],
+        'menu-icon' => 'dashicons-portfolio',
+        'public' => true,
+        'has_archive' => true,
+        'supports' => [
+            'title', 'thumbnail', 'editor', 'excerpt', 'category', 'custom-fields',
+        ]
+    ]);
+}
+add_action('init', 'products_custom_post_type');
